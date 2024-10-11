44d4dd18bce27363837737995944256d89aa2f64
lublubuterbrodi <lublubuterbrodi@gmail.com>
2024-10-11 18:11:48 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const headers = document.querySelectorAll('th');
+  const tableBody = document.querySelector('tbody');
+
+  const rows = Array.from(tableBody.querySelectorAll('tr'));
+
+  headers.forEach((header, index) => {
+    header.addEventListener('click', () => {
+      const sortedRows = [...rows].sort((a, b) => {
+        const aText = a.cells[index].textContent.trim();
+        const bText = b.cells[index].textContent.trim();
+
+        if (
+          !isNaN(parseFloat(aText.replace(/[^0-9.-]+/g, ''))) &&
+          !isNaN(parseFloat(bText.replace(/[^0-9.-]+/g, '')))
+        ) {
+          const aValue = parseFloat(aText.replace(/[^0-9.-]+/g, ''));
+          const bValue = parseFloat(bText.replace(/[^0-9.-]+/g, ''));
+
+          return aValue - bValue;
+        } else {
+          return aText.localeCompare(bText);
+        }
+      });
+
+      tableBody.innerHTML = '';
+
+      sortedRows.forEach((row) => tableBody.appendChild(row));
+    });
+  });
+});
